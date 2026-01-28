4682ad9b4ecb7e6edc1654f94fd4f240c342342a
nikeiishaa <kirananikeisha99@gmail.com>
2026-01-28 14:47:29 +0700

    membuat smart unit converter

--- Program.cs
+++ Program.cs
@@ -1,2 +1,66 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string pilihan, ulang;
+decimal hasil;
+
+decimal konversiSuhu(decimal celcius)
+{
+    return 1.8m * celcius + 32;
+}
+
+decimal konversiJarak(decimal km)
+{
+    return km * 0.62m;
+}
+
+decimal konversiMataUang(decimal idr, decimal rate = 16000)
+{
+    return idr / rate;
+}
+
+while (true)
+{
+    Console.Clear();
+    
+    Console.WriteLine("Smart Unit Converter");
+    Console.WriteLine("[1] Konversi Celcius - fahrenheit");
+    Console.WriteLine("[2] Konversi Kilometer - Miles");
+    Console.WriteLine("[3] Konversi IDR - USD");
+    Console.WriteLine("[4] Exit");
+    Console.Write("Pilihan Anda: " );
+    pilihan = Console.ReadLine();
+
+    if (pilihan == "1")
+    {
+        Console.Write("Masukkan suhu celcius: ");
+        decimal celcius = decimal.Parse(Console.ReadLine());
+
+        hasil = konversiSuhu(celcius);
+        Console.WriteLine($"Hasil: {hasil} F");
+    }
+    else if (pilihan == "2")
+    {
+        Console.Write("Masukkan kilometer: ");
+        decimal km = decimal.Parse(Console.ReadLine());
+
+        hasil = konversiJarak(km);
+        Console.WriteLine($"Hasil: {hasil} miles");
+    }
+    else if (pilihan == "3")
+    {
+        Console.Write("Masukkan IDR: ");
+        decimal idr = decimal.Parse(Console.ReadLine());
+
+        hasil = konversiMataUang(idr);
+        Console.WriteLine($"Hasil: {hasil} USD");
+    }
+    else if (pilihan == "4")
+    {
+        break;
+    }
+    else
+    {
+        Console.WriteLine("PILIHAN ANDA INVALID");
+    }
+
+    Console.ReadLine();
+}
+
